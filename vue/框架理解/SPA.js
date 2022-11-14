b8f4454de642cb158822c680bc2d0d81d3833252
kcfuler <2842961263@qq.com>
2022-11-14 16:16:00 +0800

    init the properties

---
+++ vue/框架理解/SPA.js
@@ -0,0 +1,28 @@
+class Router {
+  constructor() {
+    this.routes = {}; //存放路由path及 callback
+    this.currentUrl = '';
+
+    // 监听路由change调用相对应的路由回调
+    window.addEventListener("load", this.refresh, false);
+    window.addEventListener("hashchange", this.refresh, false);
+  }
+
+  route(path, callback) {
+    this.routes[path] = callback;
+  }
+
+  push(path) {
+    this.routes[path] && this.routes[path](); // 执行回调
+  }
+}
+
+//使用 router
+
+window.miniRouter = new Router();
+miniRouter.route('/', () => console.log('page1'))
+miniRouter.route('/page2', () => console.log('page2'))
+
+miniRouter.push('/') // page1  
+miniRouter.push('/page2') // page2  
+
