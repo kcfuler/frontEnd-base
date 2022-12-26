7ab3e154d58094978546eea5e6976632f181ebb2
kcp <2842961263@qq.com>
2022-12-26 17:04:17 +0800

    feat: finished the point free ex

--- FP/functional-light-v3/point-free/ex.js
+++ FP/functional-light-v3/point-free/ex.js
@@ -1,29 +1,47 @@
 "use strict";
 
-function output(txt) {
-	console.log(txt);
+function when(fn) {
+  return function (predict) {
+    return function (...args) {
+      if (predict(...args)) {
+        return fn(...args);
+      }
+    };
+  };
 }
 
-function printIf(shouldPrintIt) {
-	return function(msg) {
-		if (shouldPrintIt(msg)) {
-			output(msg);
-		}
-	};
+function not(fn) {
+  return function negated(...args) {
+    return !fn(...args);
+  };
 }
+
+// function output(txt) {
+//   console.log(txt);
+// }
+
+var output = console.log.bind(console);
+
+// function printIf(shouldPrintIt) {
+//   return function (msg) {
+//     if (shouldPrintIt(msg)) {
+//       output(msg);
+//     }
+//   };
+// }
+
+var printIf = when(output);
 
 function isShortEnough(str) {
-	return str.length <= 5;
+  return str.length <= 5;
 }
 
-function isLongEnough(str) {
-	return !isShortEnough(str);
-}
+var isLongEnough = not(isShortEnough);
 
 var msg1 = "Hello";
 var msg2 = msg1 + " World";
 
-printIf(isShortEnough)(msg1);		// Hello
+printIf(isShortEnough)(msg1); // Hello
 printIf(isShortEnough)(msg2);
 printIf(isLongEnough)(msg1);
-printIf(isLongEnough)(msg2);		// Hello World
+printIf(isLongEnough)(msg2); // Hello World
