690a0abb38722dd6c7ef643dae7bf5a0bd90216a
kcp <2842961263@qq.com>
2022-12-31 22:37:14 +0800

    feat: 增加差分

--- DataStructure&algorithm/acwing_base/base_algorithm/差分.cpp
+++ DataStructure&algorithm/acwing_base/base_algorithm/差分.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 using namespace std;
 
+const int N = 100010;
+int a[N], s[N];
+int n, m;
+
+void insert(int begin, int end, int v)
+{
+  s[begin] += v;
+  s[end + 1] -= v;
+}
+
 int main()
 {
-  cout << "hello world" << endl;
+  cin >> n >> m;
+
+  for (int i = 1; i <= n; i++)
+    cin >> a[i];
+
+  for (int i = 1; i <= n; i++)
+    insert(i, i, a[i]);
+
+  while (m--)
+  {
+    int l, r, c;
+    cin >> l >> r >> c;
+
+    insert(l, r, c);
+  }
+
+  for (int i = 1; i <= n; i++)
+    s[i] = s[i - 1] + s[i];
+
+  for (int i = 1; i <= n; i++)
+    cout << s[i] << " ";
+
   return 0;
 }
