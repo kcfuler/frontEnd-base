9a132c08243d172f3410d526f981f3afc9455889
kcp <2842961263@qq.com>
2023-01-17 10:18:33 +0800

    feat: 离散化（输入输出太麻烦）没写完

---
+++ DataStructure&algorithm/acwing_base/base_algorithm/离散化.ts
@@ -0,0 +1,20 @@
+// function sumOfInterval( ops : number[][] ):void{
+//     let query = [] , add = [];
+//     let allNum = [];
+//
+//     const find = (x):number =>{
+//        let l = 0 , r = allNum.length - 1;
+//
+//        while( l < r ){
+//            let mid = Math.floor(( l + r ) / 2);
+//            if( allNum[mid] >= x ) r = mid;
+//            else l = mid + 1;
+//        }
+//        return r + 1; // +1 是配合构造前缀和
+//     }
+//
+//     ops.forEach(op => {
+//
+//     })
+//
+// }
